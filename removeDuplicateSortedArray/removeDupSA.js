8f1e0acba51fd85c289522ff32038e5dbc6e0d7f
Abhishek-1310 <kumaryadavabhi1999@gmail.com>
2026-01-07 20:25:32 +0530

    remove duplicate from sorted array and square and sort an array

---
+++ removeDuplicateSortedArray/removeDupSA.js
@@ -0,0 +1,10 @@
+var removeDuplicatesFromSortedArray = function (nums) {
+    let tag = 1;
+    for (let i = 1; i < nums.length; i++) {
+        if (nums[i] != nums[i - 1]) {
+            nums[tag] = nums[i];
+            tag++;
+        }
+    }
+    return tag
+};
